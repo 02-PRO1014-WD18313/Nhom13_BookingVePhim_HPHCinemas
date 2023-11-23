c8eb7950d73a3cc5f92ea98db3238d19df4502aa
sinhhung0401 <150763197+sinhhung0401@users.noreply.github.com>
2023-11-23 09:45:05 +0700

    Add files via upload

---
+++ theloaiphimchitiet.php
@@ -0,0 +1,42 @@
+<?php
+function InsertChiTietTheLoai($id_theloai,$id_phim){
+    $sql = "INSERT INTO theloaiphimchitiet(id_theloai,id_phim) VALUES ({$id_theloai},{$id_phim})";
+    execute($sql);
+}
+
+function DeleteChiTietTheLoai($id){
+    $sql = "DELETE FROM theloaiphimchitiet WHERE id_theloaichitiet = {$id}";
+    execute($sql);
+}
+
+function UpdateChiTietTheLoai($id,$id_theloai,$id_phim){
+    $sql = "UPDATE theloaiphimchitiet SET id_theloai = {$id_theloai}, id_phim = {$id_phim} WHERE id_theloaichitiet = {$id}";
+    execute($sql);
+}
+
+function SelectChiTietTheLoai(){
+    $sql = "SELECT * FROM theloaiphimchitiet";
+    return query($sql);
+}
+
+function DeleteChiTietTheLoaiBySanPham($id){
+    $sql = "DELETE FROM theloaiphimchitiet WHERE id_phim = {$id}";
+    execute($sql);
+}
+
+function DeleteChiTietTheLoaiByTheLoai($id){
+    $sql = "DELETE FROM theloaiphimchitiet WHERE id_theloai = {$id}";
+    execute($sql);
+}
+
+function UpdateChiTietTheLoaiBySanPham($id_theloai_new,$id_theloai,$id_phim){
+    $sql = "UPDATE theloaiphimchitiet SET id_theloai = {$id_theloai_new} WHERE id_phim = {$id_phim} AND id_theloai = {$id_theloai}";
+    execute($sql);
+}
+
+function DeleteChiTietTheLoaiByTheLoaiAndPhim($idTheLoai,$idPhim){
+    $sql = "DELETE FROM theloaiphimchitiet WHERE id_theloai = {$idTheLoai} AND id_phim = {$idPhim}";
+    execute($sql);
+}
+
+?>
